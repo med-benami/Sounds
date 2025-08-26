4e4792732d1abfe35ba7322967dc1424425eb732
med-benami <126074162+med-benami@users.noreply.github.com>
2025-08-27 00:44:28 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 // Liste des sons à afficher
 const TRACKS = [
-  { file: "signal_bruit_2_carre.wav", title: "Signal original", desc: "2 signaux carrés + bruit" },
-  { file: "signal_reconstruit 2 carrés  90Hz et 240Hz 5s_16000HZ_25jun.wav", title: "Reconstruction", desc: "Reconstruction par la méthode de Sirdey(Reconstruction Sinusoïde + Bruit)." },
-  { file: "signal_reconstruit_carré de fréquence 90Hz et 240Hz et de bruit_ESPRIT_16000HZ_25jun.wav", title: "Reconstruction", desc: "Reconstruction par la méthode EDS." }
+  { file: "signal_bruit_2_carre.wav", title: "Figure 18", desc: "Signal original : 2 signaux carrés + bruit" },
+  { file: "signal_reconstruit 2 carrés  90Hz et 240Hz 5s_16000HZ_25jun.wav", title: "Figure 18", desc: "Reconstruction : méthode de Sirdey(Reconstruction Sinusoïde + Bruit)." },
+  { file: "signal_reconstruit_carré de fréquence 90Hz et 240Hz et de bruit_ESPRIT_16000HZ_25jun.wav", title: "Figure 18", desc: "Reconstruction par la méthode EDS." },
+  { file: "signal_originale_200_550_1070_Hz_ESPRIT_16000HZ_26aug.wav", title: "Figure 16", desc: "Signal original : 3 sinusoids." },
+  { file: "signal_reconstruit_200_550_1070_Hz_ESPRIT_16000HZ_26aug.wav", title: "Figure 16", desc: "Signal reconstruit : 3 sinusoids." },
+  { file: "signal_carré_200Hz_N_20_ESPRIT_16000HZ_26aug.wav", title: "Figure 14", desc: "Signal originale : 20 sinusoids." },
+  { file: "signal_carré_reconstruit_200Hz_N_3_ESPRIT_16000HZ_26aug.wav", title: "Figure 14", desc: "Signal reconstruit : 3 sinusoids." },
+  { file: "signal_carré_reconstruit_200Hz_N_8_ESPRIT_16000HZ_26aug.wav", title: "Figure 14", desc: "Signal reconstruit : 8 sinusoids." },
+  { file: "signal_carré_reconstruit_200Hz_N_16_ESPRIT_16000HZ_26aug.wav", title: "Figure 14", desc: "Signal reconstruit : 16 sinusoids." }
 ];
 
 // Récupère le container
